cc329ca4fb926f65125b86d889f12e7f91646d0f
Bubka <858858+Bubka@users.noreply.github.com>
2026-03-29 18:14:19 +0200

    Rebuild assets

--- public/build/assets/bus-vrLyNCXE.js
+++
@@ -1,1 +0,0 @@
-import{ae as e}from"./app-CWkq6V2t.js";const r=e("bus",{state:()=>({migrationUri:null,decodedUri:null,inManagementMode:!1,editedGroupName:null,username:null}),actions:{}});export{r as u};
